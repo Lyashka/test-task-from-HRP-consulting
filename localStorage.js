f6ac190951721f3f1ce10b107936679bc89ff328
Lyashka <iuliana9864@gmail.com>
2020-10-14 20:23:44 +0300

    add array of cards

---
+++ localStorage.js
@@ -0,0 +1,191 @@
+export const cardsArray = [
+  {//6
+    suit: 'hearts',
+    xStart: 324,
+    yStart: 0,
+    name: '0 - 324',
+  },
+  {//7
+    suit: 'hearts',
+    xStart: 405,
+    yStart: 0,
+    name: '1 - 405',
+  },
+  {//8
+    suit: 'hearts',
+    xStart: 486,
+    yStart: 0,
+    name: '2 - 486',
+  },
+  {//9
+    suit: 'hearts',
+    xStart: 567,
+    yStart: 0,
+    name: '3 -567',
+  },
+  {//10
+    suit: 'hearts',
+    xStart: 647,
+    yStart: 0,
+    name: '4 - 647',
+  },
+
+  {//J
+    suit: 'hearts',
+    xStart: 728,
+    yStart: 0,
+  },
+  {//Q
+    suit: 'hearts',
+    xStart: 809,
+    yStart: 0,
+  },
+  {//K
+    suit: 'hearts',
+    xStart: 890,
+    yStart: 0,
+  },
+  {//A
+    suit: 'hearts',
+    xStart: 971,
+    yStart: 0,
+  },
+
+  {//6
+    suit: 'diamonds',
+    xStart: 324,
+    yStart: 118,
+  },
+  {//7
+    suit: 'diamonds',
+    xStart: 405,
+    yStart: 118,
+  },
+  {//8
+    suit: 'diamonds',
+    xStart: 486,
+    yStart: 118,
+  },
+  {//9
+    suit: 'diamonds',
+    xStart: 567,
+    yStart: 118,
+  },
+  {//10
+    suit: 'diamonds',
+    xStart: 647,
+    yStart: 118,
+  },
+  {//J
+    suit: 'diamonds',
+    xStart: 728,
+    yStart: 118,
+  },
+  {//Q
+    suit: 'diamonds',
+    xStart: 809,
+    yStart: 118,
+  },
+  {//K
+    suit: 'diamonds',
+    xStart: 890,
+    yStart: 118,
+  },
+  {//A
+    suit: 'diamonds',
+    xStart: 971,
+    yStart: 118,
+  },
+
+  {//6
+    suit: 'clubs',
+    xStart: 324,
+    yStart: 235,
+  },
+  {//7
+    suit: 'clubs',
+    xStart: 405,
+    yStart: 235,
+  },
+  {//8
+    suit: 'clubs',
+    xStart: 486,
+    yStart: 235,
+  },
+  {//9
+    suit: 'clubs',
+    xStart: 567,
+    yStart: 235,
+  },
+  {//10
+    suit: 'clubs',
+    xStart: 647,
+    yStart: 235,
+  },
+  {//J
+    suit: 'clubs',
+    xStart: 728,
+    yStart: 235,
+  },
+  {//Q
+    suit: 'clubs',
+    xStart: 809,
+    yStart: 235,
+  },
+  {//K
+    suit: 'clubs',
+    xStart: 890,
+    yStart: 235,
+  },
+  {//A
+    suit: 'clubs',
+    xStart: 971,
+    yStart: 235,
+  },
+
+  {//6
+    suit: 'spades',
+    xStart: 324,
+    yStart: 352,
+  },
+  {//7
+    suit: 'spades',
+    xStart: 405,
+    yStart: 352,
+  },
+  {//8
+    suit: 'spades',
+    xStart: 486,
+    yStart: 352,
+  },
+  {//9
+    suit: 'spades',
+    xStart: 567,
+    yStart: 352,
+  },
+  {//10
+    suit: 'spades',
+    xStart: 647,
+    yStart: 352,
+  },
+  {//J
+    suit: 'spades',
+    xStart: 728,
+    yStart: 352,
+  },
+  {//Q
+    suit: 'spades',
+    xStart: 809,
+    yStart: 352,
+  },
+  {//K
+    suit: 'spades',
+    xStart: 890,
+    yStart: 352,
+  },
+  {//A
+    suit: 'spades',
+    xStart: 971,
+    yStart: 352,
+  },
+];
